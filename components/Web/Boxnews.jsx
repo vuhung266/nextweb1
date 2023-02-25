84bea82040e4d3821a1360dfeb7e1be0c875d743
vuhung266 <mrvuhung@gmail.com>
2023-02-25 23:56:10 +0700

    aa

--- components/Web/Boxnews.jsx
+++ components/Web/Boxnews.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Boxnews = () => {
    return (
       <div className='max-w-[1240px] mx-auto py-16 text-center'>
-         <h1 className='font-bold text-2xl p-4'>Tin tức</h1>
+         <h1 className='font-bold text-2xl p-4'>Tin tức...</h1>
          <div className='grid grid-rows-none md:grid-cols-5 p-4 gap-4'>
             <div className='w-full h-full col-span-2 md:col-span-3 row-span-2 relative'>
                <Image
